df90c02b79d0464184c16a1a5816feafe0464576
tmthan <minhthan007@gmail.com>
2021-02-16 11:20:25 +0700

    add webpack

--- functions.php
+++ functions.php
@@ -10,28 +10,8 @@ add_action( 'init', 'remove_redirects' );
 
 // Load scripts
 function load_vue_scripts() {
-	wp_enqueue_script(
-		'tmthan-theme-js-1',
-		get_stylesheet_directory_uri() . '/dist/js/app.c3b12655.js',
-		array(),
-		1.0,
-		false
-	);
-
-	wp_enqueue_script(
-		'tmthan-theme-js-2',
-		get_stylesheet_directory_uri() . '/dist/js/chunk-vendors.8c8f94ae.js',
-		array(),
-		1.0,
-		false
-	);
-
-	wp_enqueue_style(
-		'tmthan-theme-css',
-		get_stylesheet_directory_uri() . '/dist/css/app.309c9433.css',
-		false,
-		1.0
-	);
+    wp_enqueue_style('vue_wordpress.css', get_template_directory_uri() . '/dist/vue-wordpress.css');
+    wp_enqueue_script('vue_wordpress.js', get_template_directory_uri() . '/dist/vue-wordpress.js', array(), null, true);
 }
 add_action( 'wp_enqueue_scripts', 'load_vue_scripts', 100 );
 add_filter( 'rest_allow_anonymous_comments', '__return_true' );
