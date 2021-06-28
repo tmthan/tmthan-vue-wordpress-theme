aa8d7844aabaea848ac77f1b5e82b22e355008f9
Thân <minhthan007@gmail.com>
2021-06-28 20:41:32 +0700

    Fix total page

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -52,7 +52,7 @@ export default {
         )
         .then((response) => {
           this.postList = response.data;
-          this.totalPage = this.roundTotalPage(response.headers["x-wp-total"]);
+          this.totalPage = response.headers["x-wp-total"];
           this.page = page;
         })
         .catch((e) => {
@@ -61,13 +61,6 @@ export default {
     },
     async goToPage(pageNum) {
       this.$router.push(`/page/${pageNum}`);
-    },
-    roundTotalPage(total) {
-      if (0 == total % this.perPage) {
-        return total / this.perPage;
-      } else {
-        return Math.round(total / this.perPage) + 1;
-      }
     },
   },
 };
