c5502feee2b25a2bd47e603e04bb414ac9d99614
tmthan <minhthan007@gmail.com>
2021-07-17 22:46:51 +0700

    change copyright

--- header.php
+++ header.php
@@ -1,13 +1,23 @@
 <?php status_header(200); ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js no-svg">
-
+<!-- 
+     ___    ___     _______     _____      _____           _________     _________
+     \  \  /  /    |       |   |     \    /     |         |         |   |         |
+      \  \/  /     |  |''''    |  |\  \  /  /|  |         |  |''''''    |  |''''''
+       \    /      |  |____    |  | \  \/  / |  |         |  |          |  |
+       /    \      |  .....|   |  |  \    /  |  |         |  |          |  |
+      /  /\  \     |  |____    |  |   \__/   |  |         |  |______    |  |______
+     /__/  \__\    |_______|   |__|          |__|         |_________|   |_________|
+-->
   <head>
     <meta
       charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="/wp-content/themes/tmthan/dist/css/tmthan.app.css" rel="preload" as="style">
     <link href="/wp-content/themes/tmthan/dist/js/tmthan.app.js" rel="preload" as="script">
+    <script src="/wp-content/themes/tmthan/dist/js/tmthan.chunk-vendors.js"></script>
+    <script src="/wp-content/themes/tmthan/dist/js/tmthan.app.js"></script>
     <link href="/wp-content/themes/tmthan/dist/js/tmthan.chunk-vendors.js" rel="preload" as="script">
     <link href="/wp-content/themes/tmthan/dist/css/tmthan.app.css" rel="stylesheet">
     <?php wp_head(); ?>
--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php get_header(); ?>
 <noscript><strong>We're sorry but wptheme doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
-<div id="app">
-<!-- 
-     ___    ___     _______     _____      _____           _________     _________
-     \  \  /  /    |       |   |     \    /     |         |         |   |         |
-      \  \/  /     |  |''''    |  |\  \  /  /|  |         |  |''''''    |  |''''''
-       \    /      |  |____    |  | \  \/  / |  |         |  |          |  |
-       /    \      |  .....|   |  |  \    /  |  |         |  |          |  |
-      /  /\  \     |  |____    |  |   \__/   |  |         |  |______    |  |______
-     /__/  \__\    |_______|   |__|          |__|         |_________|   |_________|
--->
-</div>
+<div id="app"></div>
 <?php get_footer();
