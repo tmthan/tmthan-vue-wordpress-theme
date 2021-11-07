7f162e2d1b1f62489b670f091d3d5fe0487477a9
tmthan <minhthan007@gmail.com>
2021-11-07 10:25:28 +0700

    chore: update spinner

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,24 +1,19 @@
 <template>
   <div class="loading">
-    <svg
-      class="spinner"
-      width="65px"
-      height="65px"
-      viewBox="0 0 66 66"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle
-        class="path"
-        fill="none"
-        stroke-width="6"
-        stroke-linecap="round"
-        cx="33"
-        cy="33"
-        r="30"
-      ></circle>
-    </svg>
+    <img :src="waitingIcon" alt="Lâu quá..." />
   </div>
 </template>
+<script>
+import WaitingIcon from "../assets/waiting.webp";
+export default {
+  data() {
+    return {
+      waitingIcon: WaitingIcon,
+    };
+  },
+};
+</script>
+
 <style lang="css" scoped>
 .loading {
   display: flex;
@@ -30,52 +25,6 @@
   left: 0;
   top: 0;
   background: #fff;
-}
-.loading .spinner {
-  animation: rotator 1.4s linear infinite;
-}
-.loading .path {
-  stroke-dasharray: 187;
-  stroke-dashoffset: 0;
-  transform-origin: center;
-  animation: dash 1.4s ease-in-out infinite, colors 5.6s ease-in-out infinite;
-}
-@keyframes rotator {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(270deg);
-  }
-}
-@keyframes colors {
-  0% {
-    stroke: #4285f4;
-  }
-  25% {
-    stroke: #de3e35;
-  }
-  50% {
-    stroke: #f7c223;
-  }
-  75% {
-    stroke: #1b9a59;
-  }
-  100% {
-    stroke: #4285f4;
-  }
-}
-@keyframes dash {
-  0% {
-    stroke-dashoffset: 187;
-  }
-  50% {
-    stroke-dashoffset: 46.75;
-    transform: rotate(135deg);
-  }
-  100% {
-    stroke-dashoffset: 187;
-    transform: rotate(450deg);
-  }
+  z-index: 1;
 }
 </style>
